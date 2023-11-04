33feb69a3ae2f21d133b36a31b53fde4fd62d66d
Ajam <AjamX101@gmail.com>
2023-11-04 12:47:02 +0545

    Update eget_binaries_amd_x86_64.sh

--- .github/scripts/eget_binaries_amd_x86_64.sh
+++ .github/scripts/eget_binaries_amd_x86_64.sh
@@ -431,6 +431,8 @@ fi
   #masscan
   #Doesn't work
   #eget "https://github.com/Azathothas/Static-Binaries/raw/main/masscan/masscan_linux_x86_64_gcc" --to "$HOME/bin/masscan"
+  #mergerfs
+  eget "trapexit/mergerfs" --asset "amd64" --asset "static" --to "$HOME/bin/mergerfs"
   #mgwls
   eget "trickest/mgwls" --asset "amd64" --asset "linux" --to "$HOME/bin/mgwls"  
   #micro : https://github.com/zyedidia/micro/blob/master/runtime/help/keybindings.md
