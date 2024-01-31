716535bce1f448cc49bc8ae7a522b94b617c4c97
Ajam <AjamX101@gmail.com>
2024-01-31 12:34:32 +0545

    [+] fix gh debloat script

--- .github/scripts/eget_build_binaries_amd_x86_64.sh
+++ .github/scripts/eget_build_binaries_amd_x86_64.sh
@@ -11,22 +11,22 @@
 if [ "$USER" = "runner" ] || [ "$(whoami)" = "runner" ] && [ -s "/opt/runner/provisioner" ]; then
    echo -e "\n[+] Debloating GH Runner...\n"
      #12.0 GB
-     sudo rm /usr/local/lib/android -rf 2>/dev/null
+     sudo rm "/usr/local/lib/android" -rf 2>/dev/null
      #8.2 GB
-     sudo rm /opt/hostedtoolcache/CodeQL -rf 2>/dev/null
+     sudo rm "/opt/hostedtoolcache/CodeQL" -rf 2>/dev/null
      #5.0 GB
-     sudo rm /usr/local/.ghcup -rf 2>/dev/null
+     sudo rm "/usr/local/.ghcup" -rf 2>/dev/null
      #2.0 GB
-     sudo rm /usr/share/dotnet -rf 2>/dev/null
+     sudo rm "/usr/share/dotnet" -rf 2>/dev/null
      #1.7 GB
-     sudo rm /usr/share/swift -rf 2>/dev/null
+     sudo rm "/usr/share/swift" -rf 2>/dev/null
      #1.1 GB
-     #sudo rm /usr/local/lib/node_modules -rf 2>/dev/null
+     #sudo rm "/usr/local/lib/node_modules" -rf 2>/dev/null
      #1.0 GB
-     sudo rm /usr/local/share/powershell -rf 2>/dev/null
+     sudo rm "/usr/local/share/powershell" -rf 2>/dev/null
      #500 MB
-     sudo rm /usr/local/lib/heroku -rf 2>/dev/null
-fi 
+     sudo rm "/usr/local/lib/heroku" -rf 2>/dev/null
+fi
 #-------------------------------------------------------#
 ##Get ENV:PATH
 #TMPDIRS
