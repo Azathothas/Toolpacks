2fc82737424d8aac60768e07dedb1796cccdf9ec
Azathothas <AjamX101@gmail.com>
2024-02-11 19:33:38 +0545

    +

---
+++ .github/scripts/x86_64_Linux/bins/sudo-rs.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+#-------------------------------------------------------#
+#Sanity Checks
+if [ "$BUILD" != "YES" ] || \
+   [ -z "$BINDIR" ] || \
+   [ -z "$EGET_EXCLUDE" ] || \
+   [ -z "$EGET_TIMEOUT" ] || \
+   [ -z "$GIT_TERMINAL_PROMPT" ] || \
+   [ -z "$GIT_ASKPASS" ] || \
+   [ -z "$GITHUB_TOKEN" ] || \
+   [ -z "$SYSTMP" ] || \
+   [ -z "$TMPDIRS" ]; then
+ #exit
+  echo -e "\n[+]Skipping Builds...\n"
+  exit 1
+fi
+#-------------------------------------------------------#
+
+#-------------------------------------------------------#
+##Main
+SKIP_BUILD="NO" #YES, in case of deleted repos, broken builds etc
+if [ "$SKIP_BUILD" == "NO" ]; then
+  #sudo-rs: A memory safe implementation of sudo and su. 
+     export BIN="sudo-rs" #Name of final binary/pkg/cli, sometimes differs from $REPO
+     export SOURCE_URL="https://github.com/memorysafety/sudo-rs" #github/gitlab/homepage/etc for $BIN
+     echo -e "\n\n [+] (Building | Fetching) $BIN :: $SOURCE_URL\n"
+      #Build 
+       pushd "$($TMPDIRS)" > /dev/null 2>&1 && git clone --quiet --filter "blob:none" "https://github.com/memorysafety/sudo-rs" && cd "./sudo-rs"
+       sudo apt install libpam0g-dev -y
+       export RUST_TARGET="x86_64-unknown-linux-gnu" && rustup target add "$RUST_TARGET"
+       export RUSTFLAGS="-C default-linker-libraries=yes -C prefer-dynamic=no -C embed-bitcode=yes -C lto=yes -C opt-level=3 -C debuginfo=none -C strip=symbols -C link-arg=-Wl,-S -C link-arg=-Wl,--build-id=none"
+       sed '/^\[profile\.release\]/,/^$/d' -i "./Cargo.toml" ; echo -e '\n[profile.release]\nstrip = true\nopt-level = 3\nlto = true' >> "./Cargo.toml"
+       cargo build --target "$RUST_TARGET" --release --jobs="$(($(nproc)+1))" --keep-going
+       staticx --loglevel DEBUG "./target/$RUST_TARGET/release/su" --strip "$BINDIR/su-rs"
+       staticx --loglevel DEBUG "./target/$RUST_TARGET/release/sudo" --strip "$BINDIR/sudo-rs"
+       staticx --loglevel DEBUG "./target/$RUST_TARGET/release/visudo" --strip "$BINDIR/visudo-rs" ; popd > /dev/null 2>&1
+fi
+#-------------------------------------------------------#
+
+#-------------------------------------------------------#
+##Cleanup
+#In case of zig polluted env
+unset AR CC CFLAGS CXX CXXFLAGS DLLTOOL HOST_CC HOST_CXX LDFLAGS OBJCOPY RANLIB
+#-------------------------------------------------------#
