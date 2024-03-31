307ba12374a57aa6f5208bbd04b990a933f9e4fc
Azathothas <AjamX101@gmail.com>
2024-03-31 14:50:43 +0545

    +

--- .github/scripts/aarch64_Linux/bins/quotes-escaper.sh
+++ .github/scripts/aarch64_Linux/bins/quotes-escaper.sh
@@ -21,15 +21,15 @@ fi
 ##Main
 export SKIP_BUILD="NO" #YES, in case of deleted repos, broken builds etc
 if [ "$SKIP_BUILD" == "NO" ]; then
-      #inscope : filtering URLs and domains supplied on stdin to make sure they meet one of a set of regular expressions
-     export BIN="inscope" #Name of final binary/pkg/cli, sometimes differs from $REPO
+   #quotes-escaper : Escape Single | Double Quotes
+     export BIN="quotes-escaper" #Name of final binary/pkg/cli, sometimes differs from $REPO
      export SOURCE_URL="https://github.com/Azathothas/Arsenal" #github/gitlab/homepage/etc for $BIN
      echo -e "\n\n [+] (Building | Fetching) $BIN :: $SOURCE_URL\n"
       #Build
-       pushd "$($TMPDIRS)" > /dev/null 2>&1 && mkdir -p "./inscope" && cd "./inscope"
-       curl -qfsSLJO "https://raw.githubusercontent.com/Azathothas/Arsenal/main/inscope/main.go"
-       go mod init "github.com/Azathothas/Arsenal/inscope" ; go mod tidy
-       GOOS="linux" GOARCH="arm64" CGO_ENABLED="0" go build -v -ldflags="-buildid= -s -w -extldflags '-static'" -o "./inscope" ; cp "./inscope" "$BINDIR/inscope" ; popd > /dev/null 2>&1 ; go clean -cache -fuzzcache -modcache -testcache
+       pushd "$($TMPDIRS)" > /dev/null 2>&1 && mkdir -p "./quotes-escaper" && cd "./quotes-escaper"
+       curl -qfsSLJO "https://raw.githubusercontent.com/Azathothas/Arsenal/main/quotes-escaper/main.go"
+       go mod init "github.com/Azathothas/Arsenal/quotes-escaper" ; go mod tidy
+       GOOS="linux" GOARCH="arm64" CGO_ENABLED="0" go build -v -ldflags="-buildid= -s -w -extldflags '-static'" -o "./quotes-escaper" ; cp "./quotes-escaper" "$BINDIR/quotes-escaper" ; popd > /dev/null 2>&1 ; go clean -cache -fuzzcache -modcache -testcache
 fi
 #-------------------------------------------------------#
 
--- .github/scripts/x86_64_Linux/bins/inscope.sh
+++ .github/scripts/x86_64_Linux/bins/inscope.sh
@@ -26,7 +26,7 @@ if [ "$SKIP_BUILD" == "NO" ]; then
      export SOURCE_URL="https://github.com/Azathothas/Arsenal" #github/gitlab/homepage/etc for $BIN
      echo -e "\n\n [+] (Building | Fetching) $BIN :: $SOURCE_URL\n"
       #Build
-       pushd "$($TMPDIRS)" > /dev/null 2>&1 && mkdir inscope && cd "./inscope"
+       pushd "$($TMPDIRS)" > /dev/null 2>&1 && mkdir -p "./inscope" && cd "./inscope"
        curl -qfsSLJO "https://raw.githubusercontent.com/Azathothas/Arsenal/main/inscope/main.go"
        go mod init "github.com/Azathothas/Arsenal/inscope" ; go mod tidy
        GOOS="linux" GOARCH="amd64" CGO_ENABLED="0" go build -v -ldflags="-buildid= -s -w -extldflags '-static'" -o "./inscope" ; cp "./inscope" "$BINDIR/inscope" ; popd > /dev/null 2>&1 ; go clean -cache -fuzzcache -modcache -testcache
