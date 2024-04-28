f1c95f8e6e8aa39d55031980f629e1f7155948ee
Ajam <AjamX101@gmail.com>
2024-04-28 19:16:05 +0545

    +

--- .github/scripts/x86_64_Linux/bins/fastfetch.sh
+++ .github/scripts/x86_64_Linux/bins/fastfetch.sh
@@ -25,9 +25,70 @@ if [ "$SKIP_BUILD" == "NO" ]; then
      export BIN="fastfetch" #Name of final binary/pkg/cli, sometimes differs from $REPO
      export SOURCE_URL="https://github.com/fastfetch-cli/fastfetch" #github/gitlab/homepage/etc for $BIN
      echo -e "\n\n [+] (Building | Fetching) $BIN :: $SOURCE_URL\n"
-      #Fetch
-      #This is Dynamic
-       eval "$EGET_TIMEOUT" eget "$SOURCE_URL" --asset "linux" --asset "tar.gz" "$EGET_EXCLUDE" --to "$BINDIR/$BIN"
+      #Build
+       pushd "$($TMPDIRS)" > /dev/null 2>&1
+       docker stop "alpine-builder" 2>/dev/null ; docker rm "alpine-builder" 2>/dev/null
+       docker run --privileged --net="host" --name "alpine-builder" "azathothas/alpine-builder:latest" \
+        sh -c '
+        #Setup ENV
+         tempdir="$(mktemp -d)" ; mkdir -p "$tempdir" && cd "$tempdir"
+         mkdir -p "/build-bins"
+        #Install Deps:: https://github.com/Azathothas/Toolpacks/blob/main/.github/runners/alpine-builder.dockerfile
+        #https://github.com/fastfetch-cli/fastfetch/blob/dev/.github/workflows/ci.yml
+         apk update && apk upgrade --no-interactive
+         apk add chafa-dev --latest --upgrade --no-interactive
+         apk add dconf-dev --latest --upgrade --no-interactive
+         apk add dbus-dev --latest --upgrade --no-interactive
+         apk add ddcutil-dev --latest --upgrade --no-interactive
+         apk add imagemagick-dev --latest --upgrade --no-interactive
+         apk add imagemagick-static --latest --upgrade --no-interactive
+         apk add libdrm-dev --latest --upgrade --no-interactive
+         apk add libxcb-dev --latest --upgrade --no-interactive
+         apk add libxcb-static --latest --upgrade --no-interactive
+         apk add libxrandr-dev --latest --upgrade --no-interactive
+         apk add libva-glx-dev --latest --upgrade --no-interactive
+         apk add mesa-dev --latest --upgrade --no-interactive
+         apk add networkmanager-dev --latest --upgrade --no-interactive
+         apk add opencl-dev --latest --upgrade --no-interactive
+         apk add pulseaudio-dev --latest --upgrade --no-interactive
+         apk del rpm --force --no-interactive
+         apk add samurai --latest --upgrade --no-interactive
+         apk add vulkan-loader-dev --latest --upgrade --no-interactive
+         apk add xfconf-dev --latest --upgrade --no-interactive
+         apk add wayland-dev --latest --upgrade --no-interactive
+        #Build
+         git clone --filter "blob:none" --quiet "https://github.com/fastfetch-cli/fastfetch" && cd "./fastfetch"
+         mkdir -p "./STATIC_BUILD"
+         cmake -DCMAKE_C_FLAGS="-O2 -flto=auto -static -w -pipe" \
+         -DCMAKE_EXE_LINKER_FLAGS="-static -s -Wl,-S -Wl,--build-id=none" \
+         -DCMAKE_BUILD_TYPE="Release" \
+         -DCMAKE_INSTALL_PREFIX="/usr" \
+         -DIS_MUSL="ON" \
+         -DBUILD_SHARED_LIBS="Off" \
+         -DBUILD_TESTS="Off" \
+         -DSET_TWEAK="Off" \
+         -GNinja \
+         -B "./STATIC_BUILD"
+         cmake --build "./STATIC_BUILD" --target package -j"$(($(nproc)+1))"
+        #strip & info
+         strip "./STATIC_BUILD/fastfetch" ; strip "./STATIC_BUILD/flashfetch"
+         cp "./STATIC_BUILD/fastfetch" "/build-bins/fastfetch"
+         cp "./STATIC_BUILD/flashfetch" "/build-bins/flashfetch"
+        '
+      #Copy 
+       docker cp "alpine-builder:/build-bins/fastfetch" "./fastfetch"
+       docker cp "alpine-builder:/build-bins/flashfetch" "./flashfetch"
+       #Meta 
+       file "./fastfetch" && du -sh "./fastfetch"
+       file "./flashfetch" && du -sh "./flashfetch"
+       cp "./fastfetch" "$BINDIR/fastfetch"
+       cp "./flashfetch" "$BINDIR/flashfetch"
+       #Test
+       docker run --privileged -it --rm --network="host" -v "$BINDIR:/mnt" "alpine" "/mnt/fastfetch"
+       docker run --privileged -it --rm --network="host" -v "$BINDIR:/mnt" "alpine" "/mnt/flashfetch"       
+      #Delete Containers
+       docker stop "alpine-builder" 2>/dev/null ; docker rm "alpine-builder"
+       popd > /dev/null 2>&1
 fi
 #-------------------------------------------------------#
 
