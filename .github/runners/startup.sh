fd0fd6ad91f3a39cbcb838ae5d88b19e059e1c47
Ajam <AjamX101@gmail.com>
2024-05-19 21:19:33 +0545

    +

--- .github/runners/startup.sh
+++ .github/runners/startup.sh
@@ -22,7 +22,7 @@ start_tailscaled()
  sudo systemctl enable "tailscaled.service" --now 2>/dev/null
  sudo systemctl restart "tailscaled.service" 2>/dev/null
  sudo systemctl list-unit-files --type=service | grep -i "tailscale"
- sudo systemctl status "tailscaled.service"
+ sudo systemctl status "tailscaled.service" 2>/dev/null
 }
 export -f start_tailscaled
 if command -v s6-svc &>/dev/null && [ -d "/command" ] && [ -d "/etc/s6-overlay/s6-rc.d/tailscaled" ]; then
@@ -30,10 +30,10 @@ if command -v s6-svc &>/dev/null && [ -d "/command" ] && [ -d "/etc/s6-overlay/s
    sudo "$(command -v s6-svc)" -r "/etc/s6-overlay/s6-rc.d/tailscaled" 2>/dev/null
    sleep 10
    if ! sudo pgrep -f 'tailscaled --tun=userspace-networking' >/dev/null && command -v systemctl &>/dev/null && [ -s "/lib/systemd/system/tailscaled.service" ]; then
-     start_tailscaled
+     start_tailscaled 2>/dev/null
    fi
 elif ! sudo pgrep -f 'tailscaled --tun=userspace-networking' >/dev/null && command -v systemctl &>/dev/null && [ -s "/lib/systemd/system/tailscaled.service" ]; then
-   start_tailscaled
+   start_tailscaled 2>/dev/null
 fi
 #------------------------------------------------------------------------------------#
 
