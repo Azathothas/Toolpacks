255a37684606bc6c53124497a7c8ae00341a4122
Ajam <AjamX101@gmail.com>
2024-05-19 21:36:40 +0545

    +

--- .github/runners/startup.sh
+++ .github/runners/startup.sh
@@ -14,28 +14,28 @@ if command -v systemctl &>/dev/null && [ -s "/lib/systemd/system/docker.service"
 fi
 #------------------------------------------------------------------------------------#
 
-#------------------------------------------------------------------------------------#
-#If using s6-svc (in a container), attempt to restart tailscale
-start_tailscaled()
-{
- sudo systemctl daemon-reload 2>/dev/null
- sudo systemctl enable "tailscaled.service" --now 2>/dev/null
- sudo systemctl restart "tailscaled.service" 2>/dev/null
- sudo systemctl list-unit-files --type=service | grep -i "tailscale"
- sudo systemctl status "tailscaled.service" 2>/dev/null
-}
-export -f start_tailscaled
-if command -v s6-svc &>/dev/null && [ -d "/command" ] && [ -d "/etc/s6-overlay/s6-rc.d/tailscaled" ]; then
-   timeout -k 10 20 sudo "$(command -v s6-svc)" -u "/etc/s6-overlay/s6-rc.d/tailscaled" 2>/dev/null
-   timeout -k 10 20 sudo "$(command -v s6-svc)" -r "/etc/s6-overlay/s6-rc.d/tailscaled" 2>/dev/null
-   sleep 10
-   if ! sudo pgrep -f 'tailscaled --tun=userspace-networking' >/dev/null && command -v systemctl &>/dev/null && [ -s "/lib/systemd/system/tailscaled.service" ]; then
-     start_tailscaled 2>/dev/null
-   fi
-elif ! sudo pgrep -f 'tailscaled --tun=userspace-networking' >/dev/null && command -v systemctl &>/dev/null && [ -s "/lib/systemd/system/tailscaled.service" ]; then
-   start_tailscaled 2>/dev/null
-fi
-#------------------------------------------------------------------------------------#
+##------------------------------------------------------------------------------------#
+##If using s6-svc (in a container), attempt to restart tailscale
+#start_tailscaled()
+#{
+# sudo systemctl daemon-reload 2>/dev/null
+# sudo systemctl enable "tailscaled.service" --now 2>/dev/null
+# sudo systemctl restart "tailscaled.service" 2>/dev/null
+# sudo systemctl list-unit-files --type=service | grep -i "tailscale"
+# sudo systemctl status "tailscaled.service" 2>/dev/null
+#}
+#export -f start_tailscaled
+#if command -v s6-svc &>/dev/null && [ -d "/command" ] && [ -d "/etc/s6-overlay/s6-rc.d/tailscaled" ]; then
+#   timeout -k 10 20 sudo "$(command -v s6-svc)" -u "/etc/s6-overlay/s6-rc.d/tailscaled" 2>/dev/null
+#   timeout -k 10 20 sudo "$(command -v s6-svc)" -r "/etc/s6-overlay/s6-rc.d/tailscaled" 2>/dev/null
+#   sleep 10
+#   if ! sudo pgrep -f 'tailscaled --tun=userspace-networking' >/dev/null && command -v systemctl &>/dev/null && [ -s "/lib/systemd/system/tailscaled.service" ]; then
+#     start_tailscaled 2>/dev/null
+#   fi
+#elif ! sudo pgrep -f 'tailscaled --tun=userspace-networking' >/dev/null && command -v systemctl &>/dev/null && [ -s "/lib/systemd/system/tailscaled.service" ]; then
+#   start_tailscaled 2>/dev/null
+#fi
+##------------------------------------------------------------------------------------#
 
 #------------------------------------------------------------------------------------#
 ##Read from --env-file=runner.env
