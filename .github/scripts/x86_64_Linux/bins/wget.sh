6d3c340fb7d4ec93a1b052424f456abb4046ddfd
Azathothas <AjamX101@gmail.com>
2024-09-06 13:39:42 +0545

    +

--- .github/scripts/x86_64_Linux/bins/wget.sh
+++ .github/scripts/x86_64_Linux/bins/wget.sh
@@ -26,8 +26,8 @@ if [ "$SKIP_BUILD" == "NO" ]; then
      export SOURCE_URL="https://www.gnu.org/software/wget/"
      echo -e "\n\n [+] (Building | Fetching) $BIN :: $SOURCE_URL\n"
       #fetch
-       eget "https://github.com/supriyo-biswas/static-builds" --tag "wget" --asset "x86_64" --file "wget" --to "$BINDIR/wget"
-       cp "$BINDIR/wget" "$BASEUTILSDIR/wget"     
+       #eget "https://github.com/supriyo-biswas/static-builds" --tag "wget" --asset "x86_64" --file "wget" --to "$BINDIR/wget"
+       #cp "$BINDIR/wget" "$BASEUTILSDIR/wget"     
       ##Build 
       # pushd "$($TMPDIRS)" >/dev/null 2>&1
       # NIXPKGS_ALLOW_BROKEN="1" NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM="1" nix-build '<nixpkgs>' --attr "pkgsStatic.wget" --cores "$(($(nproc)+1))" --max-jobs "$(($(nproc)+1))" --log-format bar-with-logs
@@ -35,6 +35,36 @@ if [ "$SKIP_BUILD" == "NO" ]; then
       # cp "./result/bin/wget" "$BINDIR/wget"
       # sudo cp "./result/bin/wget" "$BASEUTILSDIR/wget"
       # nix-collect-garbage >/dev/null 2>&1 ; popd >/dev/null 2>&1
+      ##Build: https://github.com/leleliu008/ppkg-formula-repository-official-core/blob/master/formula/wget.yml
+       pushd "$($TMPDIRS)" >/dev/null 2>&1
+       docker stop "alpine-builder" 2>/dev/null ; docker rm "alpine-builder" 2>/dev/null
+       docker run --privileged --net="host" --name "alpine-builder" "azathothas/alpine-builder:latest" \
+        bash -c '
+        #Setup ENV
+         mkdir -p "/build-bins" && pushd "$(mktemp -d)" >/dev/null 2>&1
+        #https://github.com/leleliu008/ppkg
+        #https://github.com/leleliu008/ppkg-package-manually-build/blob/master/.github/workflows/manually-build-for-linux-musl.yml
+         sudo curl -qfsSL "https://raw.githubusercontent.com/leleliu008/ppkg/master/ppkg" -o "/usr/local/bin/ppkg" && sudo chmod a+x "/usr/local/bin/ppkg"
+         ppkg setup --syspm ; ppkg setup ; ppkg update
+         ppkg formula-repo-add "main-core" "https://github.com/leleliu008/ppkg-formula-repository-official-core" --enable
+         ppkg formula-repo-conf "main-core" --url="https://github.com/leleliu008/ppkg-formula-repository-official-core" --enable --pin ; ppkg formula-repo-list
+        #Build
+         ppkg install "wget" --profile="release" -j "$(($(nproc)+1))" --static
+         ppkg tree "wget" --dirsfirst -L 5
+        #Copy
+         ppkg tree "wget" --dirsfirst -L 1 | grep -o "/.*/.*" 2>/dev/null | tail -n1 | xargs realpath | xargs -I{} sudo rsync -av --copy-links --exclude="*/" "{}/bin/." "/build-bins/."
+         popd >/dev/null 2>&1
+        '
+      #Copy & Meta
+       docker cp "alpine-builder:/build-bins/." "$(pwd)/"
+       find "." -maxdepth 1 -type f -exec file -i "{}" \; | grep "application/.*executable" | cut -d":" -f1 | xargs realpath
+       #Meta
+       find "." -maxdepth 1 -type f -exec sh -c 'file "{}"; du -sh "{}"' \;
+       sudo rsync -av --copy-links --exclude="*/" "./." "$BINDIR"
+       cp "$BINDIR/wget" "$BASEUTILSDIR/wget"  
+      #Delete Containers
+       docker stop "alpine-builder" 2>/dev/null ; docker rm "alpine-builder"
+       popd >/dev/null 2>&1
 fi
 #-------------------------------------------------------#
 
