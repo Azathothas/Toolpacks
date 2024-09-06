652174ca91370a8a6e792705af4d9530af937444
Azathothas <AjamX101@gmail.com>
2024-09-06 13:09:20 +0545

    [+] xdo + xdotool https://github.com/Azathothas/Toolpacks/issues/29

--- .github/scripts/aarch64_Linux/bins/xdotool.sh
+++ .github/scripts/aarch64_Linux/bins/xdotool.sh
@@ -25,12 +25,36 @@ if [ "$SKIP_BUILD" == "NO" ]; then
      export BIN="xdotool"
      export SOURCE_URL="https://github.com/jordansissel/xdotool"
      echo -e "\n\n [+] (Building | Fetching) $BIN :: $SOURCE_URL\n"
-     ##Install 
-      pushd "$($TMPDIRS)" >/dev/null 2>&1
-      sudo apt update -y && sudo apt install xdotool -y
-      sudo cp "$(which xdotool)" "./xdotool" && sudo chown -R "$(whoami):$(whoami)" "$(realpath .)" && chmod -R 755 "$(realpath .)"
-      strip "./xdotool" ; staticx --loglevel DEBUG "./xdotool" --strip "$BINDIR/xdotool"
-      file "$BINDIR/xdotool" ; du -sh "$BINDIR/xdotool" ; popd >/dev/null 2>&1
+      ##Build:
+       pushd "$($TMPDIRS)" >/dev/null 2>&1
+       docker stop "alpine-builder" 2>/dev/null ; docker rm "alpine-builder" 2>/dev/null
+       docker run --privileged --net="host" --name "alpine-builder" "azathothas/alpine-builder:latest" \
+        bash -c '
+        #Setup ENV
+         mkdir -p "/build-bins" && pushd "$(mktemp -d)" >/dev/null 2>&1
+        #https://github.com/leleliu008/ppkg
+        #https://github.com/leleliu008/ppkg-package-manually-build/blob/master/.github/workflows/manually-build-for-linux-musl.yml
+         sudo curl -qfsSL "https://raw.githubusercontent.com/leleliu008/ppkg/master/ppkg" -o "/usr/local/bin/ppkg" && sudo chmod a+x "/usr/local/bin/ppkg"
+         ppkg setup --syspm ; ppkg setup ; ppkg update
+         ppkg formula-repo-add "main-core" "https://github.com/leleliu008/ppkg-formula-repository-official-core" --enable
+         ppkg formula-repo-conf "main-core" --url="https://github.com/leleliu008/ppkg-formula-repository-official-core" --enable --pin ; ppkg formula-repo-list 
+        #Build
+         ppkg install "xdotool" --profile="release" -j "$(($(nproc)+1))" --static
+         ppkg tree "xdotool" --dirsfirst -L 5
+        #Copy
+         ppkg tree "xdotool" --dirsfirst -L 1 | grep -o "/.*/.*" 2>/dev/null | tail -n1 | xargs realpath |xargs -I{} sudo rsync -av --copy-links --exclude="*/" "{}/bin/." "/build-bins/."
+         popd >/dev/null 2>&1
+        '
+      #Copy & Meta
+       docker cp "alpine-builder:/build-bins/." "$(pwd)/"
+       find "." -maxdepth 1 -type f -exec file -i "{}" \; | grep "application/.*executable" | cut -d":" -f1 | xargs realpath
+       #Meta
+       find "." -maxdepth 1 -type f -exec sh -c 'file "{}"; du -sh "{}"' \;
+       sudo rsync -av --copy-links --exclude="*/" "./." "$BINDIR"
+      #Delete Containers
+       docker stop "alpine-builder" 2>/dev/null ; docker rm "alpine-builder"
+       popd >/dev/null 2>&1
+
 fi
 #-------------------------------------------------------#
 
--- .github/scripts/x86_64_Linux/bins/xdo.sh
+++ .github/scripts/x86_64_Linux/bins/xdo.sh
@@ -25,12 +25,35 @@ if [ "$SKIP_BUILD" == "NO" ]; then
      export BIN="xdo"
      export SOURCE_URL="https://github.com/baskerville/xdo"
      echo -e "\n\n [+] (Building | Fetching) $BIN :: $SOURCE_URL\n"
-     ##Install 
-      pushd "$($TMPDIRS)" >/dev/null 2>&1
-      sudo apt update -y && sudo apt install xdo -y
-      sudo cp "$(which xdo)" "./xdo" && sudo chown -R "$(whoami):$(whoami)" "$(realpath .)" && chmod -R 755 "$(realpath .)"
-      strip "./xdo" ; staticx --loglevel DEBUG "./xdo" --strip "$BINDIR/xdo"
-      file "$BINDIR/xdo" ; du -sh "$BINDIR/xdo" ; popd >/dev/null 2>&1
+      ##Build:
+       pushd "$($TMPDIRS)" >/dev/null 2>&1
+       docker stop "alpine-builder" 2>/dev/null ; docker rm "alpine-builder" 2>/dev/null
+       docker run --privileged --net="host" --name "alpine-builder" "azathothas/alpine-builder:latest" \
+        bash -c '
+        #Setup ENV
+         mkdir -p "/build-bins" && pushd "$(mktemp -d)" >/dev/null 2>&1
+        #https://github.com/leleliu008/ppkg
+        #https://github.com/leleliu008/ppkg-package-manually-build/blob/master/.github/workflows/manually-build-for-linux-musl.yml
+         sudo curl -qfsSL "https://raw.githubusercontent.com/leleliu008/ppkg/master/ppkg" -o "/usr/local/bin/ppkg" && sudo chmod a+x "/usr/local/bin/ppkg"
+         ppkg setup --syspm ; ppkg setup ; ppkg update
+         ppkg formula-repo-add "main-core" "https://github.com/leleliu008/ppkg-formula-repository-official-core" --enable
+         ppkg formula-repo-conf "main-core" --url="https://github.com/leleliu008/ppkg-formula-repository-official-core" --enable --pin ; ppkg formula-repo-list 
+        #Build
+         ppkg install "xdo" --profile="release" -j "$(($(nproc)+1))" --static
+         ppkg tree "xdo" --dirsfirst -L 5
+        #Copy
+         ppkg tree "xdo" --dirsfirst -L 1 | grep -o "/.*/.*" 2>/dev/null | tail -n1 | xargs realpath |xargs -I{} sudo rsync -av --copy-links --exclude="*/" "{}/bin/." "/build-bins/."
+         popd >/dev/null 2>&1
+        '
+      #Copy & Meta
+       docker cp "alpine-builder:/build-bins/." "$(pwd)/"
+       find "." -maxdepth 1 -type f -exec file -i "{}" \; | grep "application/.*executable" | cut -d":" -f1 | xargs realpath
+       #Meta
+       find "." -maxdepth 1 -type f -exec sh -c 'file "{}"; du -sh "{}"' \;
+       sudo rsync -av --copy-links --exclude="*/" "./." "$BINDIR"
+      #Delete Containers
+       docker stop "alpine-builder" 2>/dev/null ; docker rm "alpine-builder"
+       popd >/dev/null 2>&1
 fi
 #-------------------------------------------------------#
 
