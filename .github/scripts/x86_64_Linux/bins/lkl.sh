cbd2beecb02c772674e0d7161f1e97b182d0610f
Azathothas <AjamX101@gmail.com>
2024-09-07 23:44:55 +0545

    [+] lklfuse (ppkg) https://github.com/Azathothas/Toolpacks/issues/25

--- .github/scripts/x86_64_Linux/bins/lkl.sh
+++ .github/scripts/x86_64_Linux/bins/lkl.sh
@@ -28,15 +28,35 @@ if [ "$SKIP_BUILD" == "NO" ]; then
      #-------------------------------------------------------#
       ##Fetch: https://github.com/ading2210/shimboot-binaries/issues/1
        mkdir -p "$BASEUTILSDIR/lkl"
-       eval "$EGET_TIMEOUT" eget "https://github.com/ading2210/shimboot-binaries" --asset "tar.gz" --file "lklfuse" --to "$BASEUTILSDIR/lkl/lklfuse"
-      ###Build (lkl)
-       #pushd "$($TMPDIRS)" >/dev/null 2>&1
-       #NIXPKGS_ALLOW_BROKEN="1" NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM="1" nix-build '<nixpkgs>' --attr "pkgsStatic.lkl" --cores "$(($(nproc)+1))" --max-jobs "$(($(nproc)+1))" --log-format bar-with-logs
-       #mkdir -p "$BASEUTILSDIR/lkl" ; sudo rsync -av --copy-links "./result-bin/bin/." "$BASEUTILSDIR/lkl"
-       sudo chown -R "$(whoami):$(whoami)" "$BASEUTILSDIR/lkl/" && chmod -R 755 "$BASEUTILSDIR/lkl/"
-       #Strip
-       find "$BASEUTILSDIR/lkl" -type f ! -name "*.AppImage" -exec strip --strip-debug --strip-dwo --strip-unneeded --preserve-dates "{}" \; 2>/dev/null
-       nix-collect-garbage >/dev/null 2>&1 ; popd >/dev/null 2>&1
+       #eval "$EGET_TIMEOUT" eget "https://github.com/ading2210/shimboot-binaries" --asset "tar.gz" --file "lklfuse" --to "$BASEUTILSDIR/lkl/lklfuse"
+      ##Build:
+       pushd "$($TMPDIRS)" >/dev/null 2>&1
+       docker stop "alpine-builder" 2>/dev/null ; docker rm "alpine-builder" 2>/dev/null
+       docker run --privileged --net="host" --name "alpine-builder" "alpine:latest" \
+        sh -c '
+        #Setup ENV
+         mkdir -p "/build-bins" && cd "$(mktemp -d)" >/dev/null 2>&1
+         apk update && apk upgrade --no-interactive 2>/dev/null
+        #CoreUtils 
+         apk add autoconf binutils build-base clang clang-static cmake coreutils croc curl elfutils file gawk gcc gettext git iputils jq linux-tools make mold moreutils musl musl-dev musl-utils nano ncdu perl pkgconfig procps python3 rsync sudo tar util-linux xz zig zstd 7zip --no-interactive 2>/dev/null
+        #https://github.com/leleliu008/ppkg
+        #https://github.com/leleliu008/ppkg-package-manually-build/blob/master/.github/workflows/manually-build-for-linux-musl.yml
+         sudo curl -qfsSL "https://raw.githubusercontent.com/leleliu008/ppkg/master/ppkg" -o "/usr/local/bin/ppkg" && sudo chmod a+x "/usr/local/bin/ppkg"
+         ppkg setup --syspm ; ppkg setup ; ppkg update
+         ppkg formula-repo-add "main-core" "https://github.com/leleliu008/ppkg-formula-repository-official-core" --enable
+         ppkg formula-repo-conf "main-core" --url="https://github.com/leleliu008/ppkg-formula-repository-official-core" --enable --pin ; ppkg formula-repo-list
+        #Build
+         ppkg install "lklfuse" --profile="release" -j "$(($(nproc)+1))" --static
+         ppkg tree "lklfuse" --dirsfirst -L 5
+        #Copy
+         ppkg tree "lklfuse" --dirsfirst -L 1 | grep -o "/.*/.*" 2>/dev/null | tail -n1 | xargs realpath |xargs -I{} sudo rsync -av --copy-links --exclude="*/" "{}/bin/." "/build-bins/."
+        '
+      #Copy & Meta
+       docker cp "alpine-builder:/build-bins/." "$(pwd)/"
+       find "." -maxdepth 1 -type f -exec file -i "{}" \; | grep "application/.*executable" | cut -d":" -f1 | xargs realpath
+       #Meta
+       find "." -maxdepth 1 -type f -exec sh -c 'file "{}"; du -sh "{}"' \;
+       sudo rsync -av --copy-links --exclude="*/" "./." "$BASEUTILSDIR/lkl/"
       #-------------------------------------------------------#
       ##Meta
        file "$BASEUTILSDIR/lkl/"*
