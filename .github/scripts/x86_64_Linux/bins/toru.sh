1b670f67ea39990563b29dbc33c39fa4a6828989
Azathothas <AjamX101@gmail.com>
2024-09-19 09:23:49 +0545

    [+] Toru

---
+++ .github/scripts/x86_64_Linux/bins/toru.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+#-------------------------------------------------------#
+#Sanity Checks
+if [ "$BUILD" != "YES" ] || \
+   [ -z "$BINDIR" ] || \
+   [ -z "$EGET_EXCLUDE" ] || \
+   [ -z "$EGET_TIMEOUT" ] || \
+   [ -z "$GIT_TERMINAL_PROMPT" ] || \
+   [ -z "$GIT_ASKPASS" ] || \
+   [ -z "$GITHUB_TOKEN" ] || \
+   [ -z "$SYSTMP" ] || \
+   [ -z "$TMPDIRS" ]; then
+ #exit
+  echo -e "\n[+]Skipping Builds...\n"
+  exit 1
+fi
+#-------------------------------------------------------#
+
+#-------------------------------------------------------#
+##Main
+export SKIP_BUILD="NO" #YES, in case of deleted repos, broken builds etc
+if [ "$SKIP_BUILD" == "NO" ]; then
+    #toru : Bittorrent streaming CLI for anime torrents, real-time with no waiting for downloads.
+     export BIN="toru"
+     export SOURCE_URL="https://github.com/sweetbbak/toru"
+     echo -e "\n\n [+] (Building | Fetching) $BIN :: $SOURCE_URL\n"
+      #Build (alpine-musl)
+       pushd "$($TMPDIRS)" >/dev/null 2>&1
+       docker stop "alpine-builder" 2>/dev/null ; docker rm "alpine-builder" 2>/dev/null
+       docker run --privileged --net="host" --name "alpine-builder" "azathothas/alpine-builder:latest" \
+        bash -c '
+        #Setup ENV
+         mkdir -p "/build-bins" && pushd "$(mktemp -d)" >/dev/null 2>&1
+        #Build
+         git clone --quiet --filter "blob:none" "https://github.com/sweetbbak/toru" && cd "./toru"
+         GOOS="linux" GOARCH="amd64" CGO_ENABLED="1" CGO_CFLAGS="-O2 -flto=auto -fPIE -fpie -static -w -pipe" go build -v -trimpath -buildmode="pie" -ldflags="-s -w -buildid= -linkmode=external -extldflags '\''-s -w -static-pie -Wl,--build-id=none'\''" "./cmd/toru"
+        #strip & info
+         find "." -maxdepth 1 -type f -exec file -i "{}" \; | grep "application/.*executable" | cut -d":" -f1 | xargs realpath | xargs -I {} cp --force {} /build-bins/
+         popd >/dev/null 2>&1
+        '
+      #Copy
+       docker cp "alpine-builder:/build-bins/." "$(pwd)/" ; find "." -maxdepth 1 -type f -exec file -i "{}" \; | grep "application/.*executable" | cut -d":" -f1 | xargs realpath
+       #Meta
+       find "." -maxdepth 1 -type f -exec sh -c 'file "{}"; du -sh "{}"' \;
+       sudo rsync -av --copy-links --exclude="*/" "./." "$BINDIR"
+      #Delete Containers
+       docker stop "alpine-builder" 2>/dev/null ; docker rm "alpine-builder"
+       popd >/dev/null 2>&1
+fi
+#-------------------------------------------------------#
+
+#-------------------------------------------------------#
+##Cleanup
+unset SKIP_BUILD ; export BUILT="YES"
+#In case of zig polluted env
+unset AR CC CFLAGS CXX CPPFLAGS CXXFLAGS DLLTOOL HOST_CC HOST_CXX LDFLAGS LIBS OBJCOPY RANLIB
+#In case of go polluted env
+unset GOARCH GOOS CGO_ENABLED CGO_CFLAGS
+#PKG Config
+unset PKG_CONFIG_PATH PKG_CONFIG_LIBDIR PKG_CONFIG_SYSROOT_DIR PKG_CONFIG_SYSTEM_INCLUDE_PATH PKG_CONFIG_SYSTEM_LIBRARY_PATH
+#-------------------------------------------------------#
