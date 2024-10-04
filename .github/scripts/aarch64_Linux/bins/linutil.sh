a106361e15f62be3baa0efaf8264687969aec361
Azathothas <AjamX101@gmail.com>
2024-10-04 20:12:07 +0545

    [+] new pkgs

---
+++ .github/scripts/aarch64_Linux/bins/linutil.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+#-------------------------------------------------------#
+#Sanity Checks
+if [ "${BUILD}" != "YES" ] || \
+   [ -z "${BINDIR}" ] || \
+   [ -z "${EGET_EXCLUDE}" ] || \
+   [ -z "${EGET_TIMEOUT}" ] || \
+   [ -z "${GIT_TERMINAL_PROMPT}" ] || \
+   [ -z "${GIT_ASKPASS}" ] || \
+   [ -z "${GITHUB_TOKEN}" ] || \
+   [ -z "${SYSTMP}" ] || \
+   [ -z "${TMPDIRS}" ]; then
+ #exit
+  echo -e "\n[+]Skipping Builds...\n"
+  exit 1
+fi
+#-------------------------------------------------------#
+
+#-------------------------------------------------------#
+##Main
+export SKIP_BUILD="NO" #YES, in case of deleted repos, broken builds etc
+if [ "${SKIP_BUILD}" == "NO" ]; then
+  #linutil : Chris Titus Tech's Distro-agnostic Toolbox designed to simplify everyday Linux tasks
+     export BIN="linutil"
+     export SOURCE_URL="https://github.com/ChrisTitusTech/linutil"
+     echo -e "\n\n [+] (Building | Fetching) ${BIN} :: ${SOURCE_URL} [$(TZ='UTC' date +'%A, %Y-%m-%d (%I:%M:%S %p)') UTC]\n"
+      #Fetch
+       eval "$EGET_TIMEOUT" eget "$SOURCE_URL" ---asset "linutil" --asset "aarch64" --asset "^.sh" --to "$BINDIR/linutil"
+fi
+#-------------------------------------------------------#
+
+#-------------------------------------------------------#
+##Cleanup
+unset SKIP_BUILD ; export BUILT="YES"
+#In case of zig polluted env
+unset AR CC CFLAGS CXX CPPFLAGS CXXFLAGS DLLTOOL HOST_CC HOST_CXX LDFLAGS LIBS OBJCOPY RANLIB
+#In case of go polluted env
+unset GOARCH GOOS CGO_ENABLED CGO_CFLAGS
+#PKG Config
+unset PKG_CONFIG_PATH PKG_CONFIG_LIBDIR PKG_CONFIG_SYSROOT_DIR PKG_CONFIG_SYSTEM_INCLUDE_PATH PKG_CONFIG_SYSTEM_LIBRARY_PATH
+#-------------------------------------------------------#
---
+++ .github/scripts/aarch64_Linux/bins/sic.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+#-------------------------------------------------------#
+#Sanity Checks
+if [ "${BUILD}" != "YES" ] || \
+   [ -z "${BINDIR}" ] || \
+   [ -z "${EGET_EXCLUDE}" ] || \
+   [ -z "${EGET_TIMEOUT}" ] || \
+   [ -z "${GIT_TERMINAL_PROMPT}" ] || \
+   [ -z "${GIT_ASKPASS}" ] || \
+   [ -z "${GITHUB_TOKEN}" ] || \
+   [ -z "${SYSTMP}" ] || \
+   [ -z "${TMPDIRS}" ]; then
+ #exit
+  echo -e "\n[+]Skipping Builds...\n"
+  exit 1
+fi
+#-------------------------------------------------------#
+
+#-------------------------------------------------------#
+##Main
+export SKIP_BUILD="NO" #YES, in case of deleted repos, broken builds etc
+if [ "${SKIP_BUILD}" == "NO" ]; then
+    #sic: Accessible image processing and conversion from the terminal using image-rs/image
+     export BIN="sic"
+     export SOURCE_URL="https://github.com/foresterre/sic"
+     echo -e "\n\n [+] (Building | Fetching) ${BIN} :: ${SOURCE_URL} [$(TZ='UTC' date +'%A, %Y-%m-%d (%I:%M:%S %p)') UTC]\n"
+      #Build
+       pushd "$($TMPDIRS)" >/dev/null 2>&1
+       docker stop "alpine-builder" 2>/dev/null ; docker rm "alpine-builder" 2>/dev/null
+       docker run --privileged --net="host" --name "alpine-builder" --pull="always" "azathothas/alpine-builder:latest" \
+        bash -l -c '
+        #Setup ENV
+         mkdir -p "/build-bins" && pushd "$(mktemp -d)" >/dev/null 2>&1
+         source "$HOME/.cargo/env"
+         export RUST_TARGET="aarch64-unknown-linux-musl"
+         rustup target add "${RUST_TARGET}"
+         export RUSTFLAGS="-C target-feature=+crt-static -C default-linker-libraries=yes -C link-self-contained=yes -C prefer-dynamic=no -C embed-bitcode=yes -C lto=yes -C opt-level=3 -C debuginfo=none -C strip=symbols -C linker=clang -C link-arg=-fuse-ld=$(which mold) -C link-arg=-Wl,--Bstatic -C link-arg=-Wl,--static -C link-arg=-Wl,-S -C link-arg=-Wl,--build-id=none"
+        #Build
+         git clone --filter "blob:none" --quiet "https://github.com/foresterre/sic" && cd "./sic"
+         echo -e "\n[+] Target: ${RUST_TARGET}\n"
+         echo -e "\n[+] Flags: ${RUSTFLAGS}\n"
+         sed "/^\[profile\.release\]/,/^$/d" -i "./Cargo.toml" ; echo -e "\n[profile.release]\nstrip = true\nopt-level = 3\nlto = true" >> "./Cargo.toml"
+         rm rust-toolchain* 2>/dev/null
+         cargo build --target "${RUST_TARGET}" --release --jobs="$(($(nproc)+1))" --keep-going
+         find "./target/${RUST_TARGET}/release" -maxdepth 1 -type f -exec file -i "{}" \; | grep "application/.*executable" | cut -d":" -f1 | xargs realpath | xargs -I {} rsync -achvL "{}" "/build-bins/"
+         find "/build-bins/" -type f -exec objcopy --remove-section=".comment" --remove-section=".note.*" "{}" \;
+         find "/build-bins/" -type f ! -name "*.no_strip" -exec strip --strip-debug --strip-dwo --strip-unneeded --preserve-dates "{}" \; 2>/dev/null
+         file "/build-bins/"* && du -sh "/build-bins/"*
+         popd >/dev/null 2>&1
+        '
+      #Copy & Meta
+       docker cp "alpine-builder:/build-bins/." "$(pwd)/"
+       find "." -maxdepth 1 -type f -exec file -i "{}" \; | grep "application/.*executable" | cut -d":" -f1 | xargs realpath
+       #Meta
+       find "." -maxdepth 1 -type f -print | xargs -I {} sh -c 'file {}; b3sum {}; sha256sum {}; du -sh {}'
+       sudo rsync -av --copy-links --exclude="*/" "./." "$BINDIR"
+      #Delete Containers
+       docker stop "alpine-builder" 2>/dev/null ; docker rm "alpine-builder"
+       popd >/dev/null 2>&1
+fi
+#-------------------------------------------------------#
+
+#-------------------------------------------------------#
+##Cleanup
+unset SKIP_BUILD ; export BUILT="YES"
+#In case of zig polluted env
+unset AR CC CFLAGS CXX CPPFLAGS CXXFLAGS DLLTOOL HOST_CC HOST_CXX LDFLAGS LIBS OBJCOPY RANLIB
+#In case of go polluted env
+unset GOARCH GOOS CGO_ENABLED CGO_CFLAGS
+#PKG Config
+unset PKG_CONFIG_PATH PKG_CONFIG_LIBDIR PKG_CONFIG_SYSROOT_DIR PKG_CONFIG_SYSTEM_INCLUDE_PATH PKG_CONFIG_SYSTEM_LIBRARY_PATH
+#-------------------------------------------------------#
