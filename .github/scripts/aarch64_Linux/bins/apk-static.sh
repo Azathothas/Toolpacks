5a052a0528f684761a39d5d1c024fe08633798af
Azathothas <AjamX101@gmail.com>
2024-10-17 09:45:59 +0545

    [+] apk-static https://github.com/ruanformigoni/flatimage/issues/16

---
+++ .github/scripts/aarch64_Linux/bins/apk-static.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+#-------------------------------------------------------#
+#Sanity Checks
+if [ "${BUILD}" != "YES" ] || \
+   [ -z "${BINDIR}" ] || \
+   [ -z "${EGET_EXCLUDE}" ] || \
+   [ -z "${EGET_TIMEOUT}" ] || \
+   [ -z "${GIT_TERMINAL_PROMPT}" ] || \
+   [ -z "${GIT_ASKPASS}" ] || \
+   [ -z "${GITHUB_TOKEN}" ] || \
+   [ -z "${SYSTMP}" ] || \
+   [ -z "${TMPDIRS}" ]; then
+ #exit
+  echo -e "\n[+]Skipping Builds...\n"
+  exit 1
+fi
+#-------------------------------------------------------#
+
+#-------------------------------------------------------#
+##Main
+export SKIP_BUILD="NO" #YES, in case of deleted repos, broken builds etc
+if [ "${SKIP_BUILD}" == "NO" ]; then
+    #apk-static : Alpine Package Keeper (apk)
+     export BIN="apk-static"
+     export SOURCE_URL="https://gitlab.alpinelinux.org/alpine/apk-tools"
+     echo -e "\n\n [+] (Building | Fetching) ${BIN} :: ${SOURCE_URL} [$(TZ='UTC' date +'%A, %Y-%m-%d (%I:%M:%S %p)') UTC]\n"
+      #Fetch
+       curl -qfsSL "https://gitlab.alpinelinux.org/api/v4/projects/5/packages/generic//"$(curl -qfsSL "https://gitlab.alpinelinux.org/api/v4/projects/5/repository/tags" | jq -r '.[0].name' | tr -d '[:space:]')"/aarch64/apk.static" -o "${BINDIR}/apk-static"
+       objcopy --remove-section=".comment" --remove-section=".note.*" "${BINDIR}/apk-static"
+       strip --strip-debug --strip-dwo --strip-unneeded -R ".comment" -R ".gnu.version" "${BINDIR}/apk-static"
+       realpath "${BINDIR}/apk-static" | xargs -I {} sh -c 'file {}; b3sum {}; sha256sum {}; du -sh {}'
+fi
+#-------------------------------------------------------#
+
+#-------------------------------------------------------#
+##Cleanup
+unset SKIP_BUILD ; export BUILT="YES"
+#In case of zig polluted env
+unset AR CC CFLAGS CXX CPPFLAGS CXXFLAGS DLLTOOL HOST_CC HOST_CXX LDFLAGS LIBS OBJCOPY RANLIB
+#In case of go polluted env
+unset GOARCH GOOS CGO_ENABLED CGO_CFLAGS
+#PKG Config
+unset PKG_CONFIG_PATH PKG_CONFIG_LIBDIR PKG_CONFIG_SYSROOT_DIR PKG_CONFIG_SYSTEM_INCLUDE_PATH PKG_CONFIG_SYSTEM_LIBRARY_PATH
+#-------------------------------------------------------#
