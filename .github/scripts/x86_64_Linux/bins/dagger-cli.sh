72ff1ddb683a7df3b84e9e5cf73c9c76c592b3f6
Ajam <AjamX101@gmail.com>
2024-11-27 13:07:47 +0545

    fix name

--- .github/scripts/x86_64_Linux/bins/dagger-cli.sh
+++ .github/scripts/x86_64_Linux/bins/dagger-cli.sh
@@ -21,12 +21,12 @@ fi
 ##Main
 export SKIP_BUILD="NO" #YES, in case of deleted repos, broken builds etc
 if [ "${SKIP_BUILD}" == "NO" ]; then
-    #dagger :  An engine to run your pipelines in containers
-     export BIN="dagger" #Name of final binary/pkg/cli, sometimes differs from $REPO
+    #dagger-cli : An engine to run your pipelines in containers 
+     export BIN="dagger-cli" #Name of final binary/pkg/cli, sometimes differs from $REPO
      export SOURCE_URL="https://github.com/dagger/dagger" #github/gitlab/homepage/etc for $BIN
      echo -e "\n\n [+] (Building | Fetching) ${BIN} :: ${SOURCE_URL} [$(TZ='UTC' date +'%A, %Y-%m-%d (%I:%M:%S %p)') UTC]\n"
       #Fetch
-       eval "$EGET_TIMEOUT" eget "$SOURCE_URL" --asset "amd64" --asset "linux" "$EGET_EXCLUDE" --to "$BINDIR/$BIN"
+       eval "$EGET_TIMEOUT" eget "$SOURCE_URL" --asset "amd64" --asset "linux" "$EGET_EXCLUDE" --to "$BINDIR/dagger-cli"
 fi
 #-------------------------------------------------------#
 
