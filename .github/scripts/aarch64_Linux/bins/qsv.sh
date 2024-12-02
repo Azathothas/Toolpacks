a97e5845cc0fa3c2d72eb9330a170262d88c6051
Azathothas <AjamX101@gmail.com>
2024-12-02 15:54:33 +0545

    fix qsv

--- .github/scripts/aarch64_Linux/bins/qsv.sh
+++ .github/scripts/aarch64_Linux/bins/qsv.sh
@@ -19,18 +19,49 @@ fi
 
 #-------------------------------------------------------#
 ##Main
-export SKIP_BUILD="NO" #YES, in case of deleted repos, broken builds etc
+export SKIP_BUILD="NO"
 if [ "${SKIP_BUILD}" == "NO" ]; then
-    #qsv : CSVs sliced, diced & analyzed.
-     export BIN="qsv" #Name of final binary/pkg/cli, sometimes differs from $REPO
-     export SOURCE_URL="https://github.com/jqnatividad/qsv" #github/gitlab/homepage/etc for $BIN
+    #qsv: Blazing-fast Data-Wrangling toolkit
+     export BIN="qsv"
+     export SOURCE_URL="https://github.com/dathere/qsv"
      echo -e "\n\n [+] (Building | Fetching) ${BIN} :: ${SOURCE_URL} [$(TZ='UTC' date +'%A, %Y-%m-%d (%I:%M:%S %p)') UTC]\n"
       #Build
-       pushd "$($TMPDIRS)" >/dev/null 2>&1 && git clone --quiet --filter "blob:none" "https://github.com/jqnatividad/qsv" && cd "./qsv"
-       export RUST_TARGET="aarch64-unknown-linux-musl" && rustup target add "$RUST_TARGET"
-       export RUSTFLAGS="-C target-feature=+crt-static -C default-linker-libraries=yes -C link-self-contained=yes -C prefer-dynamic=no -C embed-bitcode=yes -C lto=yes -C opt-level=3 -C debuginfo=none -C strip=symbols -C linker=clang -C link-arg=-fuse-ld=$(which mold) -C link-arg=-Wl,--Bstatic -C link-arg=-Wl,--static -C link-arg=-Wl,-S -C link-arg=-Wl,--build-id=none"
-       sed '/^\[profile\.release\]/,/^$/d' -i "./Cargo.toml" ; echo -e '\n[profile.release]\nstrip = true\nopt-level = 3\nlto = true' >> "./Cargo.toml"
-       cargo build --target "$RUST_TARGET" --release --bin qsv --features="feature_capable" --jobs="$(($(nproc)+1))" --keep-going ; cp "./target/$RUST_TARGET/release/qsv" "$BINDIR/qsv" ; popd >/dev/null 2>&1
+       pushd "$($TMPDIRS)" >/dev/null 2>&1
+       docker stop "alpine-builder" 2>/dev/null ; docker rm "alpine-builder" 2>/dev/null
+       docker run --privileged --net="host" --name "alpine-builder" --pull="always" "azathothas/alpine-builder:latest" \
+        bash -l -c '
+        #Setup ENV
+         mkdir -p "/build-bins" && pushd "$(mktemp -d)" >/dev/null 2>&1
+         source "$HOME/.cargo/env"
+         rustup default nightly
+         rustup component add rust-src --toolchain nightly
+         export RUST_TARGET="aarch64-unknown-linux-musl"
+         rustup target add "${RUST_TARGET}"
+         export RUSTFLAGS="-C target-feature=+crt-static -C default-linker-libraries=yes -C link-self-contained=yes -C prefer-dynamic=no -C embed-bitcode=yes -C lto=yes -C opt-level=3 -C debuginfo=none -C strip=symbols -C linker=clang -C link-arg=-fuse-ld=$(which mold) -C link-arg=-Wl,--Bstatic -C link-arg=-Wl,--static -C link-arg=-Wl,-S -C link-arg=-Wl,--build-id=none"
+        #Build
+         git clone --filter "blob:none" --quiet "https://github.com/dathere/qsv" && cd "./qsv"
+         echo -e "\n[+] Target: ${RUST_TARGET}\n"
+         echo -e "\n[+] Flags: ${RUSTFLAGS}\n"
+         sed "/^\[profile\.release\]/,/^$/d" -i "./Cargo.toml" ; echo -e "\n[profile.release]\nstrip = true\nopt-level = 3\nlto = true" >> "./Cargo.toml"
+         rm rust-toolchain* 2>/dev/null
+         #https://github.com/dathere/qsv/blob/master/docs/FEATURES.md
+         #polars makes it slooow
+         cargo build --target "${RUST_TARGET}" --features "feature_capable,to" --release --jobs="$(($(nproc)+1))" --keep-going
+         find "./target/${RUST_TARGET}/release" -maxdepth 1 -type f -exec file -i "{}" \; | grep "application/.*executable" | cut -d":" -f1 | xargs realpath | xargs -I {} rsync -achvL "{}" "/build-bins/"
+         find "/build-bins/" -type f -exec objcopy --remove-section=".comment" --remove-section=".note.*" "{}" \;
+         find "/build-bins/" -type f ! -name "*.no_strip" -exec strip --strip-debug --strip-dwo --strip-unneeded --preserve-dates "{}" \; 2>/dev/null
+         file "/build-bins/"* && du -sh "/build-bins/"*
+         popd >/dev/null 2>&1
+        '
+      #Copy & Meta
+       docker cp "alpine-builder:/build-bins/." "$(pwd)/"
+       find "." -maxdepth 1 -type f -exec file -i "{}" \; | grep "application/.*executable" | cut -d":" -f1 | xargs realpath
+       #Meta
+       find "." -maxdepth 1 -type f -print | xargs -I {} sh -c 'file {}; b3sum {}; sha256sum {}; du -sh {}'
+       sudo rsync -av --copy-links --exclude="*/" "./." "${BINDIR}"
+      #Delete Containers
+       docker stop "alpine-builder" 2>/dev/null ; docker rm "alpine-builder"
+       popd >/dev/null 2>&1
 fi
 #-------------------------------------------------------#
 
